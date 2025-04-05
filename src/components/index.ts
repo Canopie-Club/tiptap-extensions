2ccccc2bbd12a3225d5892aad3d2719bf59525af
Adam Howard <adam.thomas.howard@gmail.com>
2025-04-05 12:07:38 +0200

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,12 @@
 import SlashMenuComponent from "./SlashMenu.vue";
 import RendererComponent from "./Renderer.vue";
+import { defineComponent } from "vue";
 
 // Export the raw component
-export const SlashMenu: typeof SlashMenuComponent = SlashMenuComponent;
-export const Renderer: typeof RendererComponent = RendererComponent;
+export const SlashMenu = SlashMenuComponent as ReturnType<
+  typeof defineComponent
+>;
+export const Renderer = RendererComponent as ReturnType<typeof defineComponent>;
 
 // Also re-export as default if needed
 export default { SlashMenu: SlashMenuComponent, Renderer: RendererComponent };
